5d99c1b024e2afdc3c7509f7bc407db0422b41e6
JoshuaH-sudo <joshyboy111@protonmail.com>
2023-01-26 17:43:45 +1100

    update sftp snippet code

--- code_snippets/file_transfer_to_sftp_refactor/file_transfer.tsx
+++ code_snippets/file_transfer_to_sftp_refactor/file_transfer.tsx
@@ -1,8 +1,16 @@
-//// Old Version 
+//// Old Version of the sftp plugin, originally named file_transfer 
 
-//The massive state later becomes refactored into smaller chucks and 
-//is separated out to components that will manage states internally, 
-//while any common states will be put into a context that is shared to all components.
+/**
+  * All the UI elements and logic handling for this plugin and component are in this one component.
+  * The entire file_transfer component was over 1100 lines long,
+  * after the refactor the main component was less than 100 lines and no child component went over 300+ lines.
+  */
+
+/**
+  * The massive state later becomes refactored into smaller chucks and 
+  * was separated out to components that will manage their states internally, 
+  * while any common states was put into a context to be shared to all child components.
+  */
     this.state = {
       mouseX: 0,
       mouseY: 0,
@@ -42,4 +50,86 @@
       search_query: ''
     };
 
-//...
+//The setup/mount and teardown/unmount handlers
+  componentWillUnmount(): void {
+    //In the later refactored version, the async/await syntax was used to handle promises.
+    if (this.state.connection != null) SFTP.endConnection(this.state.connection).catch(() => null);
+  }
+  
+  componentDidMount(): void {
+    //need to set up the input to have a identifying class name for the window change shortcut
+    document
+      .getElementById(`container-${this.props.window_id}`)
+      .getElementsByClassName('euiFieldSearch')[0].className += ' main_input';
+
+    getCurrentUserInfo()
+      .then((result) => {
+        this.user_id = result._id;
+      })
+      .catch(console.error);
+    this.initialize_Sftp_connection();
+  }
+
+ return (
+  //...
+  <>
+  //Section handled the navigation of the file system and was put into Table_navigation.tsx
+    <EuiButtonIcon
+      size="xs"
+      id="home"
+      aria-label="home"
+      iconType="home"
+      onClick={() => this.retrieveDirectoryListings('')}
+    />
+
+    <EuiButtonIcon
+      aria-label="refresh"
+      size="xs"
+      iconType="refresh"
+      onClick={() => this.refresh()}
+    />
+
+    <EuiBreadcrumbs breadcrumbs={this.currentNavPath()} />
+
+    <NewDirTextField
+      create={(newDir: string) => {
+        this.setState({ showMenu: false }, () => this.makeDirectory(newDir));
+      }}
+    />
+
+    <EuiFilePicker
+      id="fileUpload"
+      multiple
+      disabled={this.state.isLoading}
+      display="default"
+      initialPromptText="File(s) upload"
+      onChange={onReceivingFiles}
+    />
+    //...
+    //This table was moved into Dir_table.tsx
+      <EuiInMemoryTable
+        items={this.state.directoryView}
+        rowHeader="type"
+        columns={dirViewInfo}
+        search={search}
+        pagination={this.state.pagination}
+        error={this.state.errorLoading}
+        sorting={sorting}
+        loading={this.state.isLoading}
+        onTableChange={this.onTableChange}
+      />
+  //...
+  //This rendered a floating popup menu that provided table actions. 
+  //It was later put into Context_menu.tsx that was then controlled by Dir_table.tsx
+  <Fragment>
+    {this.state.showMenu
+      ? this.ContextMenu(
+          this.state.contextMenuXpos,
+          this.state.contextMenuYpos,
+          this.state.contextMenuItem
+        )
+      : ''}
+  </Fragment>
+  //..
+</>
+);
--- code_snippets/file_transfer_to_sftp_refactor/sftp.tsx
+++ code_snippets/file_transfer_to_sftp_refactor/sftp.tsx
@@ -1,6 +1,7 @@
 /// Refactored Version
 //useSftp.tsx
-//A hook that holds the state variables for the sftp connection and all of its methods.
+
+//A hook that holds the common shared state variables and manages the sftp connection and all of its methods.
 const [sftp_state, set_sftp_state] = useState<Sftp_state>({
     listing: [],
     cwd: '/',
@@ -14,7 +15,31 @@ const [sftp_state, set_sftp_state] = useState<Sftp_state>({
     window_id
   });
 
-  ...
+//The setup/mount and teardown/unmount handlers
+  useEffect(() => {
+    initialize_Sftp_connection();
+  }, []);
+
+  //Need to wait for the connection_id to be defined before setting the unmount side effect to close the connection.
+  useEffect(() => {
+    return () => {
+      if (sftp_state.connection_id != null) endConnection();
+    };
+  }, [sftp_state.connection_id]);
+
+//...
+ return {
+    sftp_state: sftp_state,
+    sftp_actions: {
+      initialize_Sftp_connection,
+      endConnection,
+      retrieveDirectoryListings,
+      cd_to_path,
+      makeDirectory,
+      //...
+    },
+    credential_modal
+  };
 
 //Sftp.tsx
 //The main component
@@ -24,28 +49,30 @@ const [sftp_state, set_sftp_state] = useState<Sftp_state>({
     window_id,
     close_window
   );
+  //Unique states that are not shared are stored in their own state next to the component they will be used in.
   const [pagination, set_pagination] = useState({
     pageSize: 25
   });
   const [search_query, set_search_query] = useState('');
 
-	...
-  
+return (
+	//...
+  //This provider shares the common state variables across all its child components.
   <sftp_context.Provider value={{ sftp_state, sftp_actions }}>
-
-  ...
-  
+    //...
+    //This component provides UI elements to navigate through the remote host file system.
+    //Will also update the search_query state for Dir_table
     <Table_navigation />
-
-	...
-
-	<Dir_table
-    table_props={{
-      search: search,
-      pagination: pagination,
-      onTableChange: onTableChange
-    }}
-  />
-
-	...
+    //...
+    //This will display the result (files, directories, etc.) from the sftp connection
+    <Dir_table
+      table_props={{
+        search: search,
+        pagination: pagination,
+        onTableChange: onTableChange
+      }}
+    />
+    //...
   </sftp_context.Provider>
+  //...
+)
